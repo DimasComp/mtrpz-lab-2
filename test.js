22e7c8ff07fbc4faaf6488a638b459ef317d2fa1
Demchyk Dmytro <d.demchyk.im21@kpi.ua>
2024-03-27 16:25:20 +0200

    Add new test case

--- test.js
+++ test.js
@@ -8,6 +8,14 @@ describe('convertMarkdown', () => {
     assert.include(html, '<p>Hello</p>\n<p>This is <b>Markdown</b></p>');
   });
 
+  it('should understand all markouts', () => {
+    const markdown = '_Hello_\n\n`This` ```is``` **Markdown**';
+    const out = convertMarkdown(markdown, 'stdout');
+    assert.include(out, '\x1b[3mHello\x1b[0m');
+    assert.include(out, '\x1b[7mThis\x1b[0m \x1b[7mis\x1b[0m');
+    assert.include(out, '\x1b[1mMarkdown\x1b[0m');
+  });
+
   it('should convert Markdown to stdout', () => {
     const markdown = 'Hello\n\nThis is **Markdown**';
     const out = convertMarkdown(markdown, 'stdout');
